cdc23ca24929d4d2b6313ac8c54d7d3887bbfb6f
Laura Junco <l.juncogomez@celonis.com>
2022-07-01 17:15:59 +0200

    manage-observable-subscriptions-with-unsubscribe

--- index.js
+++ index.js
@@ -10,17 +10,27 @@ const observable = new Observable(subscriber => {
     let count = 0;
     const id = setInterval(()=> {
         subscriber.next(count++)
-        subscriber.complete()
     }, 1000)
    
-    //returns the cleanup function
-    //it runs after complete() is run
     return () => { 
         console.log("clear interval");
         clearInterval(id)
     }
 }) 
 
-console.log('before');
-observable.subscribe(observer)
-console.log('after');
+//subscribe returns a subscription variable
+const subscription = observable.subscribe(observer);
+
+//we can subscribe as many times as we need to an observable
+const subscription2 = observable.subscribe(observer);
+
+//if we unsubscribe from sub1 sub2 is unsubscribed automatically too
+subscription.add(subscription2)
+
+//tells the observable to stop the work they are doing
+// cleans up all resources being used by the observable
+// the clean up method is run, the observable is not completed
+setTimeout(() => {
+    subscription.unsubscribe()
+    // subscription2.unsubscribe()
+}, 5000)
