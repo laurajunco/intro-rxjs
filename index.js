f5d4b01e88ab1b21817fb7b4cabe539381995e53
Laura Junco <l.juncogomez@celonis.com>
2022-07-01 17:48:40 +0200

    of() and range()

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 // CHAPTER 2: Observable creation operators
 
-import { fromEvent } from "rxjs";
-
-/** Creation operators start the observable pipeline
- * 
- */
+import { of, range } from "rxjs";
 
  const observer = {
     next: (value) => console.log('next', value),
     error:(err) => console.log('error', err),
     complete:() => console.log('Completed!')
 }
-// create data streams from DOM events
-const source$ = fromEvent(document, 'click');
 
-//subscribe so the observable emits data
-const sub1 = source$.subscribe(observer)
+// Emits each number in sequence before completing
+//synchrounously
+// no flattening up of the values 
+const source$ = of([1], 2, 3, 4, 5);
 
-//if we subscribe twice we get double emitions
-const sub2 = source$.subscribe(observer)
+//emits each value in the range 3 - 7
+const range$ = range(3, 5)
 
-setTimeout(() => {
-    console.log('unsubscribe sub2')
-    sub2.unsubscribe()
-}, 5000)
+source$.subscribe(observer)
+
+range$.subscribe(observer)
